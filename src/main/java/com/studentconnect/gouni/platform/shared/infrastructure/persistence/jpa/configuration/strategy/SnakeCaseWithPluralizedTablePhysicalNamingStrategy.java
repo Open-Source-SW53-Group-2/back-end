77c8dabba0832f17d032ab6c433e149385ff86a9
Alessandro David <alessandrodp2222@gmail.com>
2024-11-15 10:28:01 -0500

    feat: added code optimizatiosn for GoUni backend

--- src/main/java/com/studentconnect/gouni/platform/shared/infrastructure/persistence/jpa/configuration/strategy/SnakeCaseWithPluralizedTablePhysicalNamingStrategy.java
+++ src/main/java/com/studentconnect/gouni/platform/shared/infrastructure/persistence/jpa/configuration/strategy/SnakeCaseWithPluralizedTablePhysicalNamingStrategy.java
@@ -4,31 +4,32 @@ import org.hibernate.boot.model.naming.Identifier;
 import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
 import org.hibernate.engine.jdbc.env.spi.JdbcEnvironment;
 
+import static io.github.encryptorcode.pluralize.Pluralize.pluralize;
+
 public class SnakeCaseWithPluralizedTablePhysicalNamingStrategy implements PhysicalNamingStrategy {
     @Override
     public Identifier toPhysicalCatalogName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
-        return this.toSnakeCase(identifier);
+        return toSnakeCase(identifier);
     }
 
     @Override
     public Identifier toPhysicalSchemaName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
-        return this.toSnakeCase(identifier);
+        return toSnakeCase(identifier);
     }
 
     @Override
     public Identifier toPhysicalTableName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
-
-        return this.toSnakeCase(this.toPlural(identifier));
+        return toSnakeCase(toPlural(identifier));
     }
 
     @Override
     public Identifier toPhysicalSequenceName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
-        return this.toSnakeCase(identifier);
+        return toSnakeCase(identifier);
     }
 
     @Override
     public Identifier toPhysicalColumnName(Identifier identifier, JdbcEnvironment jdbcEnvironment) {
-        return this.toSnakeCase(identifier);
+        return toSnakeCase(identifier);
     }
 
     private Identifier toSnakeCase(final Identifier identifier) {
@@ -37,9 +38,7 @@ public class SnakeCaseWithPluralizedTablePhysicalNamingStrategy implements Physi
         }
         final String regex = "([a-z])([A-Z])";
         final String replacement = "$1_$2";
-        final String newName = identifier.getText()
-                .replaceAll(regex, replacement)
-                .toLowerCase();
+        final String newName = identifier.getText().replaceAll(regex, replacement).toLowerCase();
         return Identifier.toIdentifier(newName);
     }
 
