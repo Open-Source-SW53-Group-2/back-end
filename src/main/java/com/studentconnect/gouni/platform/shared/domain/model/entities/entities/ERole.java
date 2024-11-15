4611aa02ccbd2a589238ebb076c33228c08b2d5a
SamuelValeraGarces <u202111952@upc.edu.pe>
2024-11-14 20:57:26 -0500

    Add entities

---
+++ src/main/java/com/studentconnect/gouni/platform/shared/domain/model/entities/entities/ERole.java
@@ -0,0 +1,6 @@
+package com.studentconnect.gouni.platform.iam.domain.model.entities;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_ADMIN
+}
---
+++ src/main/java/com/studentconnect/gouni/platform/shared/domain/model/entities/entities/Role.java
@@ -0,0 +1,18 @@
+package com.studentconnect.gouni.platform.iam.domain.model.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+}
+
